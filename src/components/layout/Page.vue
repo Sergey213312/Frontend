a6fa32f27431fc4c3debc1ebf25b75405b189b17
Sergey213312 <135626589+Sergey213312@users.noreply.github.com>
2023-06-19 22:15:22 +0800

    Add files via upload

---
+++ src/components/layout/Page.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    title?: string
+    paddingless: boolean
+  }>(),
+  {
+    paddingless: false
+  }
+)
+</script>
+
+<template>
+  <section
+    class="bg-movie-black flex h-full min-w-0 flex-1 flex-col overflow-y-auto"
+  >
+    <div class="mx-4 my-9 flex justify-between">
+      <h1 v-if="title" class="text-4xl font-weight-900 font-bold text-white">
+        {{ title }}
+      </h1>
+      <h1 v-else class="text-4xl font-weight-900 font-bold text-white">
+        <slot name="title" />
+      </h1>
+      <div>
+        <slot name="header" />
+      </div>
+    </div>
+    <div :class="paddingless ? '' : 'mx-4'">
+      <slot />
+    </div>
+  </section>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Page from '../components/layout/Page.vue'
+import Tabs from '../components/Tab.vue'
+import MainBanner from '../components/MainBanner.vue'
+import ContentRow from '../components/ContentRow.vue'
+import FlashNews from '../components/FlashNews.vue'
+import MovieFeed from '../components/MovieFeed.vue'
+import { tabsMock } from '../mocks/tabs'
+const activeTab = ref<string>(tabsMock[0].id)
+const onTabChange = (tabId: string) => {
+  activeTab.value = tabId
+}
+</script>
+
+<template>
+  <page paddingless>
+    <template #title> Movie </template>
+    <div class="mx-4">
+      <tabs
+        :tabs="tabsMock"
+        :active-tab="activeTab"
+        @on-tab-change="onTabChange"
+      />
+      <content-row>
+        <main-banner />
+        <div class="ml-10 max-w-md w-full">
+          <flash-news />
+        </div>
+      </content-row>
+    </div>
+    <content-row class="flex-col">
+      <h1 class="mx-4 text-2xl text-white font-bold">Special for you</h1>
+      <movie-feed />
+    </content-row>
+  </page>
+</template>
